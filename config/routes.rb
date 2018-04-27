c944047dc3e69ea4f922b8e594d6b5b71f473239
jin_o <sstaw@likelion.org>
2018-04-27 11:00:38 +0000

    controller view setting

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
+  root 'posts#index'
+
+  get 'posts/index'
+
+  get 'posts/new'
+
+  get 'posts/update'
+
+  get 'posts/show'
+
+  get 'posts/create'
+
+  get 'posts/edit'
+
+  get 'posts/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
